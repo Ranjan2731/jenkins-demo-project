22e967c080eaef09a69e69ec983fd0f6a4f4d2cd
Cloud User <2021wa86422@wilp.bits-pilani.ac.in>
2025-07-13 14:09:46 +0530

    changes in Hello World file success

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("This is DevOps Lab 4: " + currentTime);
+		  System.out.println("This is DevOps Lab 4:done by Ranjan " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
